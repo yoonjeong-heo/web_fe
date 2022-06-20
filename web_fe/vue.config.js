6ff51cd32fef93f7d8f2d48ba7c6f2285ec0a3b8
허윤정 <yj.wiju@gmail.com>
2022-06-20 19:22:07 +0900

    Project set

--- web_fe/vue.config.js
+++ web_fe/vue.config.js
@@ -1,3 +1,36 @@
-module.exports = {
-  lintOnSave: false
-}
+const { defineConfig } = require("@vue/cli-service");
+
+const path = require("path");
+
+module.exports = defineConfig({
+  runtimeCompiler: true,
+  outputDir: path.resolve("build"),
+  transpileDependencies: true,
+  lintOnSave: false,
+  // chainWebpack: config => {
+  //   config.module.rules.delete('eslint');
+  // },
+
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/global.scss";`,
+      },
+    },
+  },
+
+  configureWebpack: {
+    output: {
+      filename: "[name].[hash].js",
+      chunkFilename: "[name].[hash].js",
+    },
+    resolve: {
+      alias: {
+        img: path.resolve("./src/assets/images/"),
+        svg: path.resolve("./src/assets/images/svg/"),
+        dummy: path.resolve("./src/assets/images/dummy/"),
+        font: path.resolve("./src/assets/font"),
+      },
+    },
+  },
+});
